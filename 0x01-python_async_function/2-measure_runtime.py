09b7190897dddf34470578b80064216108e7c295
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-06-10 23:26:34 +0300

    Add coroutine for delaying a certain amount of time and returning it

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -5,7 +5,6 @@ from time import perf_counter
 import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
-
 def measure_time(n: int, max_delay: int) -> float:
     """Measure the total execution time of a function
     Args:
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -4,7 +4,6 @@ import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Returns a task that waits for a random number of seconds
