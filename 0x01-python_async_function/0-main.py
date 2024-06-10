27a063c9ad4c6aba73849842c43c69ee957165f5
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-06-10 23:25:58 +0300

    Add coroutine for delaying a certain amount of time and returning it

--- 0x01-python_async_function/0-main.py
+++ 0x01-python_async_function/0-main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,9 +3,7 @@
 specified delay between each call."""
 import asyncio
 from typing import List
-
 wait_random = __import__('0-basic_async_syntax').wait_random
-
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns wait_random n times with a specified delay
