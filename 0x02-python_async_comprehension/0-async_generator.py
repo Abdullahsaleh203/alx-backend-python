e53d0214152c1c6ada153b57cb3b892806a7c15b
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-06-12 19:45:20 +0300

    feat: Add async generator for looping 10 times

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,8 +3,6 @@
 import random
 import asyncio
 from typing import Generator
-
-
 async def async_generator() -> Generator[float, None, None]:
     """
     async_generator - function to loop 10 times
